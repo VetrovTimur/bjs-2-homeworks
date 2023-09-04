0ae9b92088cc67375a861be6e874a5931a02c26f
TimurVetrov <timurworkvetrov@gamil.com>
2023-09-04 20:05:05 +0300

    Correction of blots

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -2,11 +2,12 @@
 
 function solveEquation(a, b, c) {
 	let arr = [];
-  let expression = b ** 2 - 4 * a * c < 0;
-	if (expression) {
+  let expression = b ** 2 - 4 * a * c; 
+	if (expression < 0) {
 		return arr;
-	} else if (expression) {
-		arr.push((-b + Math.sqrt(d)) / (2 * a), (-b - Math.sqrt(d)) / (2 * a));
+	} else if (expression > 0) {
+		arr.push((-b + Math.sqrt(expression)) / (2 * a));
+    arr.push((-b - Math.sqrt(expression)) / (2 * a));
     return arr;
 	} else {
 		arr.push(-b / (2 * a));
@@ -19,7 +20,7 @@ function calculateTotalMortgage(percent, contribution, amount, countMonths) {
     return false;
   }
   percent = percent/100/12;
-  debt = amount - contribution;
+  let debt = amount - contribution;
   let monthlyPayment = debt * (percent + (percent / ((Math.pow((1 + percent), countMonths)) - 1)));
   let totalPayment = monthlyPayment * countMonths;
 
